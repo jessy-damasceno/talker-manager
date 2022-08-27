64c4a4b0ad0422865814b45d7916402e2f34fbc9
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-27 13:44:37 +0000

    feat: req 3

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,5 +1,14 @@
 const { Router } = require('express');
+const { generate } = require('rand-token');
 
 const loginRouter = Router();
+
+loginRouter.post('/', (_req, res) => {
+  // const { email, password } = req.body;
+
+  const token = generate(16);
+
+  res.status(200).json({ token });
+});
 
 module.exports = loginRouter;

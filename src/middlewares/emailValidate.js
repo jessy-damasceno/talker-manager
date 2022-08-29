3a82812174b5132d014b14979eefdbd1383ec137
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-29 16:01:32 -0300

    fix: emailValidate (add return)

--- src/middlewares/emailValidate.js
+++ src/middlewares/emailValidate.js
@@ -3,11 +3,11 @@ const emailValidate = (req, res, next) => {
   const regex = /^[\w-.]+@([\w]+)+.com$/g;
 
   if (!email) {
-    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!regex.test(email)) {
-    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
   next();
